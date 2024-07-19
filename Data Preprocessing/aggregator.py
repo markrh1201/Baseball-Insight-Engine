7a53f363f987befa268a849247acad812eaa7273
Mark Howard <howard.1631@buckeyemail.osu.edu>
2024-07-18 20:35:47 -0400

    Preprocessing and cleaning data completed

--- Data Preprocessing/aggregator.py
+++ Data Preprocessing/aggregator.py
@@ -4,7 +4,19 @@ batting_data = pd.read_csv("team_batting_2024.csv")
 fielding_data = pd.read_csv("team_fielding_2024.csv")
 pitching_data = pd.read_csv("team_pitching_2024.csv")
 
-merged_data = pd.merge(batting_data, fielding_data, on="teamIDfg")
-merged_data = pd.merge(merged_data, pitching_data, on="teamIDfg")
+# Merge fielding and batting data
+merged_df = batting_data.merge(fielding_data, on='teamIDfg', suffixes=('', '_df2'))
 
-merged_data.to_csv("team_stats.csv")
+# Merge the result with pitching data
+merged_df = merged_df.merge(pitching_data, on='teamIDfg', suffixes=('', '_df3'))
+
+# Identify columns with duplicates based on suffixes
+cols_to_remove = [col for col in merged_df.columns if col.endswith('_df2') or col.endswith('_df3')]
+
+# Drop the duplicate columns
+merged_df.drop(columns=cols_to_remove, inplace=True)
+
+# Save the resulting DataFrame to a new CSV file
+merged_df.to_csv('combined_team_stats.csv', index=False)
+
+
